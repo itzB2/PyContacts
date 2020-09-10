8bc83165face6e1dbe62bcfb35ff7685d5c19410
judah-b2 <66715197+judah-b2@users.noreply.github.com>
2020-09-10 19:24:14 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='vcard',  
+     version='0.1',
+     scripts=['dokr'] ,
+     author="Judah Beethoven",
+     author_email="masterofcoding360@gmail.com",
+     description="A VCF card generator",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
